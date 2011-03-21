791d9e60d8686524390f3adbd73c46c047ded070
LinkFly <linkfly1@birulevo.net>
2011-03-21 15:25:05 +0300

    script fix

--- devel/commit-merge-push.sh
+++ devel/commit-merge-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git add .
-git commit -am $1
-#git checkout master
-#git merge devel
-#git push
-#git checkout devel
+git commit -am "$1"
+git checkout master
+git merge devel
+git push
+git checkout devel
