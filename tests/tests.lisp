3f7f43625110c0b72e9b1f3fc148959362d592e0
LinkFly <linkfly1@newmail.ru>
2011-12-27 04:41:29 +0400

    done task 53. Encapsulted share code in run-tests and root-run-tests

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -106,29 +106,29 @@
   `(handler-case ,@body
      (error (err) (format t "~%ERROR: Bug in the tests code. Error: ~A~%~% ... Tests failed.~%" err))))
  
-(defun run-tests () 			 
+(defun run-tests-share (user/root start-message &optional fn-extra-tests)
   (with-error-handling
-    (let* ((parent-pid (get-proc-id))
+    (let* ((*test-mode* user/root)
+	   (parent-pid (get-proc-id))
 	   (fn-child-proc-p (lambda () (/= parent-pid (get-proc-id))))
-	   (*test-mode* :user)
 	   (prompts-file-stream (open (get-log-file)
 				      :direction :output
 				      :if-does-not-exist :create
 				      :if-exists :supersede))
 	   (*standard-output* (make-broadcast-stream prompts-file-stream *standard-output*)))
-      (princ "Tests daemonized ... ")
+      (princ start-message)
       (terpri)
-      (unwind-protect 
+      (unwind-protect
 	   (block tests 
 	     (flet ((return-if-child () 
-		      (when (/= parent-pid (get-proc-id))
+		      (when (funcall fn-child-proc-p)
 			(return-from tests t)))
 		    (daemon-status () (getf (getf (daemon-status) :internal-result) :status)))
 	       (if (handler-case 
 		       (and 
 			(progn (format t "~%try test handling of the configuration ... ") (if (test-config-handling) 
-												 (progn (format t "OK~%") t)
-												 (format t "FAIL~%")))
+											      (progn (format t "OK~%") t)
+											      (format t "FAIL~%")))
 			(progn (format t "~%try start ...~%") (daemon-cmd "start") (return-if-child) (eql daemon-share:+ex-ok+ (daemon-status)))
 			(progn (format t "~%try stop ...~%") (daemon-cmd "stop") (not (eql daemon-share:+ex-ok+ (daemon-status))))
 			(progn (format t "~%try start ...~%") (daemon-cmd "start") (return-if-child) (eql daemon-share:+ex-ok+ (daemon-status)))
@@ -136,67 +136,39 @@
 			(progn (format t "~%try start ...~%") (daemon-cmd "start") (return-if-child) (eql daemon-share:+ex-ok+ (daemon-status)))
 			(progn (format t "~%try restart ...~%") (daemon-cmd "restart") (return-if-child) (eql daemon-share:+ex-ok+ (daemon-status)))
 			(progn (format t "~%try stop ...~%")(daemon-cmd "stop") (not (eql daemon-share:+ex-ok+ (daemon-status))))
+			(if fn-extra-tests 
+			    (funcall fn-extra-tests #'return-if-child)
+			    t)
 			)
 		     (error (condition) (format t "~%ERROR: ~A~%" condition)))
 		   (format t "~% ... Tests passed.")
 		   (format t "~% ... Tests failed."))
-	       (terpri)
-	       (finish-output *standard-output*)
-	       (close *standard-output*) 
-	       (close prompts-file-stream)
-	       ))			;block tests
-	(unless (funcall fn-child-proc-p) (ensure-no-pid-file))))))
 
-(defun root-run-tests (username)
-  (with-error-handling
-    (let* ((parent-pid (get-proc-id))
-	   (fn-child-proc-p (lambda () (/= parent-pid (get-proc-id))))
-	   (*test-mode* :root)
-	   (prompts-file-stream (open (get-log-file)
-				      :direction :output
-				      :if-does-not-exist :create
-				      :if-exists :supersede))
-	   (*standard-output* (make-broadcast-stream prompts-file-stream *standard-output*)))
-      (princ "Tests daemonized (with is changing of user) ... ")
-      (terpri)
-      (unwind-protect
-	   (block tests 
-	     (flet ((return-if-child () 
-		      (when (funcall fn-child-proc-p)
-			(return-from tests t)))
-		    (daemon-status () (getf (getf (daemon-status) :internal-result) :status))
-		    (daemon-status-internal-result () (getf (daemon-status) :internal-result)))
-
-	       (if (and 
-		    (progn (format t "~%try test handling of the configuration ... ") (if (test-config-handling) 
-											     (progn (format t "OK~%") t)
-											     (format t "FAIL~%")))
-		    (progn (format t "~%try start ...~%") (daemon-cmd "start") (return-if-child) (eql daemon-share:+ex-ok+ (daemon-status)))
-		    (progn (format t "~%try stop ...~%") (daemon-cmd "stop") (not (eql daemon-share:+ex-ok+ (daemon-status))))
-		    (progn (format t "~%try start ...~%") (daemon-cmd "start") (return-if-child) (eql daemon-share:+ex-ok+ (daemon-status)))
-		    (progn (format t "~%try kill ...~%") (daemon-cmd "kill") (not (eql daemon-share:+ex-ok+ (daemon-status))))
-		    (progn (format t "~%try start ...~%") (daemon-cmd "start") (return-if-child) (eql daemon-share:+ex-ok+ (daemon-status)))
-		    (progn (format t "~%try restart ...~%") (daemon-cmd "restart") (return-if-child) (eql daemon-share:+ex-ok+ (daemon-status)))
-		    (progn (format t "~%try stop ...~%")(daemon-cmd "stop") (not (eql daemon-share:+ex-ok+ (daemon-status))))
-	      
-		    (let ((*root-daemon-conf* *root-daemon-conf*))
-		      (setf (getf *root-daemon-conf* :user) username)
-		      (format t "~%--- Try is changing of user ---~%")
-		      (and
-		       (progn (format t "~%try start ...~%") (daemon-cmd "start") (return-if-child) 
-			      (equal (list daemon-share:+ex-ok+ username)
-				     (let* ((status-second (daemon-status-internal-result))
-					    (status (getf status-second :status))
-					    (extra-status (getf status-second :extra-status)))
-				       (list status (extra-status-user extra-status)))))
-		       (progn (format t "~%try stop ...~%") (daemon-cmd "stop") (not (eql daemon-share:+ex-ok+ (daemon-status))))))
-		    )			;and
-		   (format t "~% ... Tests passed.")
-		   (format t "~% ... Tests failed."))
-	  
 	       (terpri)	
 	       (finish-output *standard-output*)
 	       (close *standard-output*)
 	       (close prompts-file-stream)
-	       ))			;block tests
+	       ))
 	(unless (funcall fn-child-proc-p) (ensure-no-pid-file))))))
+
+(defun root-extra-tests (username fn-return-if-child)
+  (flet ((daemon-status-internal-result () (getf (daemon-status) :internal-result)))
+    (let ((*root-daemon-conf* *root-daemon-conf*))
+      (setf (getf *root-daemon-conf* :user) username)
+      (format t "~%--- Try is changing of user ---~%")
+      (and
+       (progn (format t "~%try start ...~%") (daemon-cmd "start") (funcall fn-return-if-child) 	      
+	      (equal (list daemon-share:+ex-ok+ username)
+		     (let* ((status-second (daemon-status-internal-result))
+			    (status (getf status-second :status))
+			    (extra-status (getf status-second :extra-status)))
+		       (list status (extra-status-user extra-status)))))
+       (progn (format t "~%try stop ...~%") (daemon-cmd "stop") (not (eql daemon-share:+ex-ok+ (daemon-status))))))))
+
+(defun run-tests ()
+  (run-tests-share :user "Tests daemonized ... "))
+
+(defun root-run-tests (username)
+  (run-tests-share :root
+		   "Tests daemonized ... "
+		   (lambda (fn-return-if-child) (root-extra-tests username fn-return-if-child))))
