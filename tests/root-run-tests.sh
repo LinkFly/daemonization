ec4840e5f5565675c37daf4d5eed3af6f2b00e36
LinkFly <linkfly1@newmail.ru>
2012-10-22 04:48:36 +0400

    Added support in tests to run any lisp-system. Also added scripts to run tests with run-lisp (from lisp-dev-tools)

--- tests/root-run-tests.sh
+++ tests/root-run-tests.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-cd "$(dirname $0)"
-sbcl --noinform --load run-tests.lisp --eval "(root-run-tests \"$1\")"
+cd "$(dirname "$(readlink -e "$0")")"
+LISP_RUNNER=${LISP_RUNNER:-sbcl}
+NOINFORM=${NOINFORM:-'--noinform'}
+LOAD=${LOAD:-'--load'}
+EVAL=${EVAL:-'--eval'}
+$LISP_RUNNER $NOINFORM $LOAD $(pwd)/run-tests.lisp $EVAL "(root-run-tests \"$1\")"
+
